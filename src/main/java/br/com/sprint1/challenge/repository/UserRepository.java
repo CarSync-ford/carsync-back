06df8a49d852304866149a89e092987cc9828cd7
Enzoreal100 <enzoreal100@gmail.com>
2026-05-14 20:20:28 -0300

    feat: added last login on login without mfa

--- src/main/java/br/com/sprint1/challenge/repository/UserRepository.java
+++ src/main/java/br/com/sprint1/challenge/repository/UserRepository.java
@@ -2,6 +2,9 @@ package br.com.sprint1.challenge.repository;
 
 import br.com.sprint1.challenge.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByCpf(String cpf);
     Optional<User> findByEmail(String email);
+    @Modifying
+    @Query("update User u set u.lastLogin = CURRENT_TIMESTAMP where u.id = :id")
+    void updateLastLoginById(@Param("id") String id);
+
 }
