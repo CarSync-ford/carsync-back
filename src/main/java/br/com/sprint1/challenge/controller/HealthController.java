284eaf096697b43316dc70fe3de6b3ebf0e0c1ea
enzoreal100 <enzoreal100@gmail.com>
2026-05-16 03:16:40 -0300

    fix: update health to root

--- src/main/java/br/com/sprint1/challenge/controller/HealthController.java
+++ src/main/java/br/com/sprint1/challenge/controller/HealthController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/health")
 public class HealthController {
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<Map<String, String>> health() {
         return ResponseEntity.ok(Map.of("status", "healthy"));
     }
